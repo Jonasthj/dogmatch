6375dc5b3a982f329672f2145836accdee7398a5
Jonasthj <jonasthenderupjensen@gmail.com>
2022-07-05 16:16:53 +0200

    Added bulma, created endpoints

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import "./style/style.css";
+import {Navbar} from "./components/navbar";
+import {Hero} from "./components/hero";
+import {HomeContent} from "./components/homeContent";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+function Home() {
+    return (
+      <div>
+        {Hero()}
+        {HomeContent()}
+      </div>
+    );
+}
+
+function Explore()
+    {
+        return <h1>Explore</h1>;
+    }
+
+function Chat()
+    {
+        return <h1>Chat</h1>;
+    }
+
+function Profile()
+    {
+        return <h1>Profile</h1>;
+    }
+
+function Application()
+    {
+        return (
+            <BrowserRouter>
+                <Routes>
+                    <Route path={"/"} element={<Home/>}/>
+                    <Route path={"/explore"} element={<Explore/>}/>
+                    <Route path={"/chat"} element={<Chat/>}/>
+                    <Route path={"/profile"} element={<Profile/>}/>
+                </Routes>
+            </BrowserRouter>
+        );
+    }
+
+ReactDOM.render(
+    <Application/>
+, document.getElementById("app"));
