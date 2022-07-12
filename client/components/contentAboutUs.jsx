5d07a4039b025278562a393dc51cb93a03f4fdbc
Jonasthj <jonasthenderupjensen@gmail.com>
2022-07-12 11:18:09 +0200

    About us page content fix

--- client/components/contentAboutUs.jsx
+++ client/components/contentAboutUs.jsx
@@ -5,7 +5,7 @@ export function ContentAboutUs(){
         <div>
             <section className={"column"}>
                 <h1 className={"title is-1 has-text-centered mb-5"}>About us</h1>
-                <h1 className={"title is-3 has-text-centered mb-5 is-italic"}>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, laudantium!"</h1>
+                <h1 className={"subtitle is-3 has-text-centered mb-5 mt-5 is-italic"}>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, laudantium!"</h1>
             </section>
             {AboutSection("Our Mission", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, atque autem commodi consequuntur cumque doloribus et ex hic, illo illum labore minima natus obcaecati quasi qui repellat voluptates? Aliquid corporis distinctio eius, fugiat, hic in ipsam molestias nemo quidem ratione repellat temporibus ullam voluptatem! Aliquid cupiditate dolor dolore enim unde.")}
             {AboutSection("Subtitle", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, atque autem commodi consequuntur cumque doloribus et ex hic, illo illum labore minima natus obcaecati quasi qui repellat voluptates? Aliquid corporis distinctio eius, fugiat, hic in ipsam molestias nemo quidem ratione repellat temporibus ullam voluptatem! Aliquid cupiditate dolor dolore enim unde.")}
